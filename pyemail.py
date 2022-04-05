4bea240edf94fd18bf6baf025dd63f023c8b2eb2
Rohit Mandal <60247178+Ro706@users.noreply.github.com>
2022-04-05 09:01:51 +0530

    Update pyemail.py

--- pyemail.py
+++ pyemail.py
@@ -10,16 +10,9 @@ print (colored('made by Ro706','red'))
 print (colored('v.1.0','magenta'))
 port = 465  # For SSL
 smtp_server = "smtp.gmail.com"
-sender_email = 'rohitmandal21062004@gmail.com'
-x = { 'mom': 'monidipa2106@gmail.com','papa':'sudheshmandal18@gmail.com','riya':'lisabake35@gmail.com','me':'rohitmandal99091@gmail.com'}
-name = input("name :")
-for key, value in x.items():
-    if key == name:
-        a =value
-    else:
-        pass
-receiver_email = a
-password = 'rohitmandal21'
+sender_email = input('sender mail id:')
+receiver_email = input('receiver mail id:')
+password = input('password:')
 message = input('message: ')
 context = ssl.create_default_context()
 with smtplib.SMTP_SSL(smtp_server, port, context=context) as server:
